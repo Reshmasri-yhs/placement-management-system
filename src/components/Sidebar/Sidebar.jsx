6da7c925de1a260d42aeb3fa9376b70ac2eddd0c
Reshmasri-yhs <23ht1a0509@gmail.com>
2026-07-10 23:05:57 +0530

    Refactor Sidebar to use Navlink and link components

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -5,13 +5,48 @@ const Sidebar = function() {
         <div className="Sidebar">
         
         <ul>
+            <Navlink to ="/login"
+            className={({isActive})=>
+            isActive ? "active":""
+            }>
+            <li>Login</li>
+            </Navlink>
+
+            <link to = "/Dashboard">
             <li>Dashboard</li>
+            </link>
+
+            <link to = "/Home">
+            <li>Home</li>
+            </link>
+
+            <link to = "/Students">
             <li>Students</li>
+            </link>
+
+            <link to = "/Companies">
             <li>Companies</li>
+            </link>
+
+            <link to = "/Placements">
             <li>Placements</li>
+            </link>
+            
+            <link to = "/Reports">
             <li>Reports</li>
+            </link>
+            
+
+            <link to = "/Settings">
             <li>Settings</li>
+            </link>
+           
+
+            <link to = "/Logout">
             <li>Logout</li>
+            </link>
+            
+            
         </ul>
         
         </div>
