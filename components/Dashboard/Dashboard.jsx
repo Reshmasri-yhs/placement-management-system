de61d236d4c64d94a2a7a63d4aee5cd73931ff5e
Reshmasri-yhs <23ht1a0509@gmail.com>
2026-07-12 23:37:07 +0530

    Refactor Dashboard to use hooks and add interactivity
    Refactor Dashboard component to use hooks for state management and add functionality for updating student and company counts.

--- components/Dashboard/Dashboard.jsx
+++ components/Dashboard/Dashboard.jsx
@@ -1,32 +1,81 @@
 import './Dashboard.css';
+import { useState } from 'react';
 
-const Dashboard = function() {
+function Dashboard({ onWelcomeClick }) {
+    const [totalStudents, setTotalStudents] = useState(250);
+    const [companies, setCompanies] = useState(35);
+    const [placed, setPlaced] = useState(100);
+    const [pending, setPending] = useState(150);
+    //Hooks with string
+    const [name, setName] = useState("Rahul");
+    //hooks with boolean
+    const[login,setLogin] = useState(false);
+    //hooks with arrays
+    const[students,setStudents] = useState([]);
+    const[restStudent,setResetStudent] = useState(250)
+    
+    function increaseStudents() {
+        setTotalStudents(totalStudents + 1);
+    }
+
+    function increaseCompanies() {
+        setCompanies(companies + 1);
+    }
+
+    function increasePlaced() {
+        setPlaced(placed + 1);
+        setPending(pending - 1);
+    }
+
+    function increasePending() {
+        setPending(pending + 1);
+    }
+
+    function changeName() {
+        setName("Reshma");
+    }
+    
+    function addStudent(){
+        setStudents([
+            "Rahul",
+            "Ravi",
+            "Lily"
+        ]);
+    }
     return (
         <div className="main-content">
-            <div className="welcome">
-                <h3>Welcome Back!</h3>
+            <div className="welcome" onClick={onWelcomeClick}>
+                <h3>Welcome Back!{name}</h3>
+                <button onClick={changeName}>Reshma</button>
             </div>
-
+        
             <div className="cards">
                 <div className="card">
-                    <h2>250</h2>
+                    <h2>{totalStudents}</h2>
+                    <button onClick={increaseStudents}>Add Student</button>
+                    <button onClick={() => setTotalStudents(250)}>Reset Student</button>
                     <p>Total Students</p>
                 </div>
                 <div className="card">
-                    <h2>35</h2>
+                    <h2>{companies}</h2>
+                    <button onClick={increaseCompanies}>Companies</button>
                     <p>Companies</p>
                 </div>
                 <div className="card">
-                    <h2>160</h2>
+                    <h2>{placed}</h2>
+                    <button onClick={increasePlaced}>Placed</button>
                     <p>Placed</p>
                 </div>
                 <div className="card">
-                    <h2>90</h2>
+                    <h2>{pending}</h2>
+                    <button onClick={increasePending}>Pending</button>
+               
                     <p>Pending</p>
-                </div>
-            </div>
-        </div>
-    )
-};
-
+                    <p>{students.join(', ')}</p>
+                                        <button onClick={addStudent}>Students</button>
+                                    </div>
+                                </div>
+                            </div>
+                        );
+                    }
 export default Dashboard;
