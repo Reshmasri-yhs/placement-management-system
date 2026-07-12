6bc2d9c5d438f028f04239b6692bc2161f00beaf
Reshmasri-yhs <23ht1a0509@gmail.com>
2026-07-12 23:50:23 +0530

    Implement registration toggle in Home component
    Added state management to toggle between Dashboard and Register components.

--- Pages/Home.jsx
+++ Pages/Home.jsx
@@ -1,10 +1,21 @@
-import Dashboard from "../components/Dashboard/Dashboard";
-function Home(){
-    return(
+import { useState } from "react"
+import Dashboard from "../components/Dashboard/Dashboard.jsx"
+import Register from "./Registration/Register.jsx"
+
+function Home() {
+    const [showRegister, setShowRegister] = useState(false)
+
+    return (
         <div>
-            <Dashboard />
-            
+            {
+                showRegister ? (
+                    <Register />
+                ) : (
+                    <Dashboard onWelcomeClick={() => setShowRegister(true)} />
+                )
+            }
         </div>
     )
 }
-export default Home;
+
+export default Home
